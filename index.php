32f1fa658a807f12929ae59ecd498bb1d8db8a72
SirioRedGiant <Oiris1992@gmail.com>
2026-04-30 18:20:24 +0200

    consegna esercizio

--- index.php
+++ index.php
@@ -46,6 +46,7 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esercizio 1: php-hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
     <h1>
@@ -58,8 +59,40 @@ $hotels = [
             echo "$key: $value <br>";
         }
     }
+ ?>
+<table class="table table-dark table-striped-columns">
+  <thead class= "table-success">
+    <tr>
+      <?php
+      // estrazione delle chiavi dall'array per evitare di doverle inserire a mano
+      $keys = array_keys($hotels[0]);
 
+      foreach ($keys as $key) {
+        // pulizia degli "_"
+        $clean_intestazione = ucfirst(str_replace("_", " ", $key));
+      
 
+      ?>
+      <th scope="col"><?php echo $clean_intestazione ?></th>
+      <?php
+      }
+      ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($hotels as $hotel) {
     ?>
+    <tr>
+      <th scope="row"><?php echo $hotel["name"];  ?></th>
+      <td><?php echo $hotel["description"];  ?></td>
+      <td><?php echo $hotel["parking"] ? "yes" : "no"; ?></td>
+      <td><?php echo $hotel["vote"] ?></td>
+      <td><?php echo $hotel["distance_to_center"] ?></td>
+    </tr>
+    <?php
+    }
+  ?>
+  </tbody>
+</table>
 </body>
 </html>
